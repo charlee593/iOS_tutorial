035ae2ffd0e09a5fefd78e79b4fa625497b62c07
charlee593 <charlee593@gmail.com>
2017-09-08 15:38:45 -0700

    Finish beginner tutorial

--- BullEye/ViewController.swift
+++ BullEye/ViewController.swift
@@ -11,10 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 50
+    var targetValue: Int = 0
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var targetLabel : UILabel!
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +39,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert(){
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, Word", message:message, preferredStyle: .alert)
         
@@ -32,13 +48,13 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
     }
-
 
 }
 
